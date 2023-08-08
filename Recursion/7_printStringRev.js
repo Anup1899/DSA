032961ad352ffc1f803f3ec49b3624c4eaa857c2
Anup Alone <anup.purandas@lyxelandflamingo.com>
2023-08-08 23:28:56 +0530

    New Way has been added

--- Recursion/7_printStringRev.js
+++ Recursion/7_printStringRev.js
@@ -1,12 +1,24 @@
-function PrintStringReverse(str, idx = str.length-1){
+// function PrintStringReverse(str, idx = str.length-1){
 
-    if(idx === -1){
-        return;
+//     if(idx === -1){
+//         return;
+//     }
+//     console.log(str[idx]);
+//     PrintStringReverse(str, idx-1);
+// }
+
+
+function PrintStringReverse(str, idx= str.length - 1, newStr = ""){
+    if(idx === 0){
+        newStr = str[str.length -1]
+        return newStr
     }
-    console.log(str[idx]);
-    PrintStringReverse(str, idx-1);
 
+   let newS =  PrintStringReverse(str, idx-1, newStr)
+   return newS += str[str.length -1 - idx]
 }
 
-// PrintStringReverse("ABCDE")
-PrintStringReverse("")
+console.log(
+    PrintStringReverse("ABCDE")
+);
+// PrintStringReverse("")
