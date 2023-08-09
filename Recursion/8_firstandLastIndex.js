c5ffefebfddc1a0a0bebc733736427a903f52460
Anup Alone <anup.purandas@lyxelandflamingo.com>
2023-08-09 09:16:39 +0530

    8&9 Revise

--- Recursion/8_firstandLastIndex.js
+++ Recursion/8_firstandLastIndex.js
@@ -1,29 +1,51 @@
 let first= -1;
 let last = -1;
 
-function FirstAndLastIndex(str, idx=0, char){
+// function FirstAndLastIndex(str, idx=0, char){
 
+//     if(idx === str.length){
+//         console.log("First ", first);
+//         console.log("Last ", last);
+//         return;
+//     }
+
+//     if(char === str.charAt(idx)){
+
+//             if(first === -1){
+//                 first = idx;
+//                 last = idx;
+//             }
+//             else{
+//                 last = idx
+//             }
+//     }
+
+
+//     FirstAndLastIndex(str, idx+1, char)
+
+// }
+
+
+// FirstAndLastIndex("abcdeafghaijkal", 0, "a");
+
+
+function FirstAndLastIndex(str, char ,idx = 0 ){
     if(idx === str.length){
-        console.log("First ", first);
-        console.log("Last ", last);
+        console.log(str.length);
+        console.log(first);
+        console.log(last);
         return;
     }
 
-    if(char === str.charAt(idx)){
-
-            if(first === -1){
-                first = idx;
-                last = idx;
-            }
-            else{
-                last = idx
-            }
+    if(str[idx]=== char){
+        if(first === -1 && last === -1){
+            first = last = idx;
+        }
+        else{
+            last = idx;
+        }
     }
-
-
-    FirstAndLastIndex(str, idx+1, char)
-
+    FirstAndLastIndex(str, char, idx+1)
 }
 
-
-FirstAndLastIndex("abcdeafghaijkal", 0, "a");
+FirstAndLastIndex("abcdeafghaijkal", "a",0)
--- Recursion/9_checkSortedArr.js
+++ Recursion/9_checkSortedArr.js
@@ -1,16 +1,42 @@
-function checkSortedArray(arr, idx=0){
+// function checkSortedArray(arr, idx=0){
 
-    if(idx === arr.length - 1){
-        return true;
+
+//     if(idx === arr.length - 1){
+//         return true;
+//     }
+
+//     if(arr[idx] >= arr[idx+1]){
+//         return false;
+//     }
+
+//   return checkSortedArray(arr, idx+1);
+// }
+
+// console.log(
+//     checkSortedArray([1,2,2,3,4,5])
+//     );
+
+
+function checkSortedArray(arr, idx = 0){
+    if(idx === arr.length){
+        console.log(true);
+        return
     }
 
-    if(arr[idx] >= arr[idx+1]){
-        return false;
-    }
 
-  return checkSortedArray(arr, idx+1);
+    if(idx === 0){
+        checkSortedArray(arr, idx+1)
+    }
+    else{
+        if(arr[idx] > arr[idx -1]){
+        return checkSortedArray(arr, idx+1)
+        }
+        else{
+            console.log(false);
+            return
+        }
+    }
 }
 
-console.log(
-    checkSortedArray([1,2,2,3,4,5])
-    );
+checkSortedArray([1,2,3,4,5,6])
+checkSortedArray([1,2,3,4,6,5])
