fe307e3e98a1e0354bf79681c43ba55726db4cc0
Anup Alone <anup.purandas@lyxelandflamingo.com>
2023-08-23 21:51:42 +0530

    FibonaCCIi new way

--- Recursion/4_fibonacci.js
+++ Recursion/4_fibonacci.js
@@ -1,20 +1,33 @@
-function fibonacci(n){
+// function fibonacci(n){
     
-    if(n <2) return n;
-    return fibonacci(n-1) + fibonacci(n-2)
+//     if(n <2) return n;
+//     return fibonacci(n-1) + fibonacci(n-2)
     
+// }
+
+// console.log(fibonacci(6));
+
+
+// function MYfibonacci(n, prev, curr, i){
+//     if(i === n) return curr;
+
+//     return MYfibonacci(n, curr, prev+curr, i+1);
+
+// }
+
+// console.log(MYfibonacci(8, 0, 1, 2));
+
+
+function fibonacci(n, prev=0, curr=1, i=2){
+    
+    if(i===n) return curr+ prev;
+
+    console.log("n", "prev", "curr", "i");
+    console.log(n, curr, prev+ curr, i+1);
+
+    return fibonacci(n, curr, prev+ curr, i+1)
 }
 
-console.log(fibonacci(6));
-
-
-
-
-function MYfibonacci(n, prev, curr, i){
-    if(i === n) return curr;
-
-    return MYfibonacci(n, curr, prev+curr, i+1);
-
-}
-
-console.log(MYfibonacci(8, 0, 1, 2));
+console.log(
+    fibonacci(6)
+);
