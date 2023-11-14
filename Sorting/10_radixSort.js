fde1ef56abcb5bb49c2fe98e3ccc475df0989c2a
Anup-1827 <anupalone.off@gmail.com>
2023-11-14 11:59:58 +0530

    Radix

---
+++ Sorting/10_radixSort.js
@@ -0,0 +1,5 @@
+// Radix Sort follows non-comparable approach
+// Numbers are sorted by their place (unit --> tenth --> hundredth)
+// Sorting of the data has an average time complexity of nlogn using merge and quick sort
+// For some cases we want to make the sorting algorith more faster than nlogn
+// Which is possible my sorting the data by non-comparable approach
