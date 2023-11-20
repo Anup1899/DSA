5ca542cb51735c09b30f525686fa6be076659379
Anup Alone <anup.purandas@lyxelandflamingo.com>
2023-11-20 17:15:50 +0530

    Circular Linked List Started

---
+++ LinkedList/3_CricularSinglyLinkedList.js
@@ -0,0 +1,19 @@
+class Node{
+    constructor(element){
+        this.element = element;
+        this.next  = null;
+    }
+}
+
+class CircularSinglyLinkedList{
+
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    //InsertAtHead
+    InsertAtHead(){
+        
+    }
+}
