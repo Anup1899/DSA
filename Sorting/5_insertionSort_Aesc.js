3fb0477b67d1a723cf894563b2f187ba230f68b3
Anup-1827 <anupalone.off@gmail.com>
2024-04-02 15:38:59 +0530

    Dry Run had been added

--- Sorting/5_insertionSort_Aesc.js
+++ Sorting/5_insertionSort_Aesc.js
@@ -19,3 +19,32 @@ function InsertionSortAesc(arr){
 
 const sortedArr = InsertionSortAesc([7,8,3,4,1])
 console.log(sortedArr);
+
+/*
+    Iteration
+    i=0; i<5
+
+    i=1 currentEle = arr[1] = 8
+    
+        j=0
+        while(j>=0 && 8 < arr[j]) // j>=0 && 8 < 7 --------> FALSE
+        
+        arr[1] = 8
+
+    i=2 currentEle = arr[2] = 3
+
+        j=1
+        while(j>=0 && 3 < 8)
+            arr[2] = 8
+            --j = 0
+        while(j>=0 && 3 < 7)
+            arr[1] = 7
+            --j = -1
+        arr[0] = 3
+
+        [3,7,8,4,1]
+    
+
+        
+
+*/
