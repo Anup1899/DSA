3198661e3d05d1f8964c1aeb9472a2934f3cd0a0
Anup-1827 <anupalone.off@gmail.com>
2025-05-12 20:58:37 +0530

    Printwise in Python

--- BackTracking/Easy5_printBitwise.py
+++ BackTracking/Easy5_printBitwise.py
@@ -1,1 +1,15 @@
-def PrintBitwise(n, arr):
+def PrintBitwise(n, arr = None):
+    if(arr == None):
+        arr = [None] * n
+    
+    if(n <1):
+        print(arr)
+        return
+    else:
+        arr[n-1] = 0
+        PrintBitwise(n-1, arr)
+        arr[n-1] = 1
+        PrintBitwise(n-1, arr)
+
+
+PrintBitwise(3)
