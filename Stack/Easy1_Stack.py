31b92ad9205907db1f3680c3a5314e05b891f2e4
Anup-1827 <anupalone.off@gmail.com>
2025-05-26 13:24:56 +0530

    Stack Question

---
+++ Stack/Easy1_Stack.py
@@ -0,0 +1,55 @@
+class Node:
+    def  __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class Stack:
+    head = None
+    
+    def isEmpty(self):
+        return self.head == None
+    
+    def push(self, data):
+        newNode = Node(data)
+        if(self.head == None):
+            self.head = newNode
+            return
+        # Create New Node
+        # Head.next = 
+        # Current Head.next will be newnode
+        # newNode will be head
+        newNode.prev = self.head
+        self.head.next = newNode
+        self.head = newNode
+
+    def pop(self):
+        if(self.isEmpty()):
+            return -1
+        data = self.head.data
+        self.head.prev.next = None
+        self.head = self.head.prev
+        return data
+
+    def peek(self):
+        if(self.isEmpty()):
+            return -1
+        return self.head.data
+        
+
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+stack.push(5)
+stack.push(6)
+stack.push(7)
+
+print(stack.peek())
